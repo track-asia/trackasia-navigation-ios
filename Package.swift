b95591bc1e34789ff27f5fbbc51d6e5f8afdf60b
Sang Nguyen <vsang25@gmail.com>
2025-04-15 11:14:50 +0700

    #Change - Fix

--- Package.swift
+++ Package.swift
@@ -18,8 +18,8 @@ let package = Package(
         .package(url: "https://github.com/flitsmeister/mapbox-directions-swift", exact: "0.23.3"),
         .package(url: "https://github.com/mapbox/turf-swift.git", from: "2.8.0"),
         .package(url: "https://github.com/ceeK/Solar.git", exact: "3.0.1"),
-        .package(url: "https://github.com/track-asia/trackasia-gl-native-distribution.git", .branch("2.0.3"))
-        .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.53.6")
+        .package(url: "https://github.com/nicklockwood/SwiftFormat.git", from: "0.53.6"),
+        .package(url: "https://github.com/track-asia/trackasia-gl-native-distribution.git", exact: "2.0.3")
     ],
     targets: [
         .target(
